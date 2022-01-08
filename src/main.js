e73faa9f97f467c0d1ffde51e493486fdf66166c
kamila villablanca <kvillablancabaez@gmail.com>
2022-01-08 18:02:50 -0300

    despligue de una nueva seccion mediante js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import { filterAZ, filterZA, filterDataYearAsc, filterDataYearDesc, filterDataDi
 
 import data from './data/ghibli/ghibli.js';
 
-// Constantes
+// variables globales
 const dataStudioGhibli = data.films;
-const headerMain = document.getElementById('header')
+//const headerMain = document.getElementById('header')
 const containerMain = document.getElementById('main__animations');
 const footerMain = document.getElementById('footer');
 const containerAnimationes = document.getElementById('animations');
@@ -13,78 +13,76 @@ const filterLetterOrder = document.getElementById('filters__initial');
 const filterXDirector = document.getElementById('filters__director');
 const filterXProducer = document.getElementById('filters__producer');
 const filterYear = document.getElementById('filters__year');
+const body = document.querySelector('body');
 
-
-/* 
-const animation = data.films;
-let animationLength = animation.length;
-
-let theAnimation = "<section>";
-for (let i = 0; i < animationLength; i++) {
-
-    theAnimation += "<article>" +
-        `<img src="${animation[i].poster}" alt="">
-    <p <span class="negrita"> Titulo: ${animation[i].title}</p> 
-    <p> <span class="negrita">Producer: </span>${animation[i].producer}</p> 
-    <p><span class="negrita">Año: </span> ${animation[i].release_date}</p>
-    <p><span class="negrita">Score: </span> ${animation[i].rt_score}</p>`;
-    + "</article>";
-}
-theAnimation += "<section>";
-
-document.getElementById("animations").innerHTML = theAnimation; 
-*/
-
-/*
-// Recorre la data y crea las tarjetas con su respectiva información sacada de la data.
-
- const dataGhibli = data.films.map((arr) => {
-    let ghibliData = `<article class="animations__card"> <a href="#"><img class="animations__card__img" src="${arr.poster}"alt="Imagen de la película de animación"></a>
-    <h4>${arr.title}</h4>
-    <p>Año: ${arr.release_date}</p>
-    <p>Productor: ${arr.producer}</p>
-    <p>Score: ${arr.rt_score} </p>
-   </article>`;
-    return ghibliData;
-}) 
-
-
-//document.getElementById("animations").innerHTML = dataGhibli.join('');
-*/
 
 // Creación de tarjetas dinamicas que llama información del objeto.data.ghibli
 // Se crea una función que lleva dentro un forEach que recorre el parametro que le de creando así las tarjetas. Al final llamo a la función y le doy el parametro de la data del estudio ghibli para que pueda crear las tarjetas.
+
+
+
 const displayCardGhibli = (ghibliData) => {
     containerAnimationes.innerHTML = "";
     //contador de peliculas segun filtro
-
     document.getElementById("countFilms").innerHTML = `Estas visualizando: ${ghibliData.length} peliculas`;
     ghibliData.forEach((arr) => {
-
-        containerAnimationes.innerHTML += `<article id="animations__card" class="animations__card">
-        <img class="animations__card__img" src="${arr.poster}"alt="Imagen de la película de animación">
+        // Se crea el article donde ira cada tarjeta.
+        //https://developer.mozilla.org/es/docs/Web/API/Document/createElement
+        const cardAnimations = document.createElement("article"); //article para cada tarjeta
+        // Le agrego los estilos de css a cada card.
+        //https://developer.mozilla.org/es/docs/Web/API/Element/classList
+        cardAnimations.classList.add("animations__card");
+        // Se agrega el contenido a la tarjeta.
+        cardAnimations.innerHTML += `<img class="animations__card__img" src="${arr.poster}"alt="Imagen de la película de animación">
         <h4>${arr.title}</h4>
-        <p><span class="black">Año:</span> ${arr.release_date}</p>
-        <p><span class="black">Director:</span> ${arr.director}</p>
-        <p><span class="black">Productor:</span> ${arr.producer}</p>
-        <p><span class="black">Score:</span> ${arr.rt_score}</p>
-       </article>`;
+        <p>Año: ${arr.release_date}</p>
+        <p>Productor: ${arr.producer}</p>
+        <p>Score: ${arr.rt_score} </p>`;
+        // Guardo el id del objeto en una nueva constante.
+        const idObjetCard = arr.id;
+        // A la tarjeta le agrego el nuevo atributo de ID que toma el valor del id unico que trae el objeto. 
+        cardAnimations.setAttribute("id", idObjetCard);
+        // En el container(section) de las animaciones despliego las tarjetas
+        // https://developer.mozilla.org/es/docs/Web/API/Node/appendChild
+        containerAnimationes.appendChild(cardAnimations);
 
         // Tiene que ir adentro para que funcione al dar click en todas las tarjetas, incluso a las que se toman con filtro. 
-        const animationsCard = document.querySelectorAll('.animations__card');
-        for (let i = 0; i < animationsCard.length; i++) {
-            animationsCard[i].addEventListener('click', (e) => {
-                e.preventDefault();
-                containerMain.style.display = 'none';
-                //headerMain.classList.remove('header')
-            })
+
+        //Evento a la tarjeta con el id incluido y llamá a la función que crea la nueva pantalla.
+        cardAnimations.addEventListener("click", newContainer);
+
+        function newContainer() {
+            // Desaparece la pantalla donde se despliegan las tarjetas(Main)
+            containerMain.style.display = 'none';
+            footerMain.style.display = 'none';
+            //Crea el nuevo contenedor main
+            let peliContainer = document.createElement("main");
+            // Agrego el estilo para el contenedor main creado en css.
+            peliContainer.classList.add('movie__container');
+            // Creo la sección de sidebar (Bloque lateral)
+            let sideBar = document.createElement("section");
+            // Agrega clase al sidebar
+            sideBar.classList.add('movie__sidebar')
+            // Crea contenido dinamico al sidebar.
+            sideBar.innerHTML = `<h4> ${arr.title}</ >
+            <img class="movie__sidebar__img" src="${arr.poster}"
+            alt="Imagen de la pelicula seleccionada">` ;
+            let tabContainer = document.createElement("section");
+            tabContainer.classList.add('movie__mainContent')
+
+            // Despliega en la pantalla el contenedor del Main
+            body.appendChild(peliContainer);
+            // Despliega en la pantalla el contenedor del sidebar.
+            peliContainer.appendChild(sideBar);
+            // Despliega en la pantalla el contenedor donde iras las pestallas tab.
+            peliContainer.appendChild(tabContainer);
+            // Ahora hay que seguir creando las siguientes secciones basado en el contenido que tengamos en peliculas.html
         }
+
     });
 
 };
 displayCardGhibli(dataStudioGhibli);
-
-// Function remover elementos
 
 
 // Filtrar de la A a la Z y de la Z a la A
@@ -162,11 +160,3 @@ filterYear.addEventListener('change', () => {
         displayCardGhibli(filterXYear)
     }
 })
-
-
-
-
-
-
-
-
